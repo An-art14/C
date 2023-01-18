0642d043714558bd09296af49846c8cf27ff63b9
An-art14 <an_art14@mail.ru>
2023-01-18 21:05:32 +0700

    add iff

---
+++ age.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+int a;
+scanf("%d", &a);
+if ((a<=6)) {
+    printf("дошкольник\n"); }
+if ((a>=7)&&(a<=18)) {
+ printf("школьник\n"); }
+if ((a>=19)&&(a<=59)) {
+    printf("рабочий\n"); }
+if (a>=60) { 
+    printf("песионер\n"); }
+return 0;
+}
