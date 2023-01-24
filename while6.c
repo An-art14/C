bc02c7be85de81393cae960c2a6909e45dc29091
An-art14 <an_art14@mail.ru>
2023-01-24 13:24:58 +0700

    with while

---
+++ while6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+int n;
+scanf("%d", &n);
+int a;
+
+for (int i=1; i<=sqrt(n); i++){
+a=pow(i, 2);
+//if (a<=n)
+printf("%d ", a);}
+  return 0;
+}
