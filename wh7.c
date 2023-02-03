ddf1af48cee90105707262d3acb6e5ed413b7c5f
anastasiya <an_art14@mail.ru>
2023-02-03 21:33:48 +0700

    add with while

---
+++ wh7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    /*int n=0, flag=0;
+    while (n!=-9999) {
+        scanf("%d", &n);
+    if (n==2517)
+    flag=1;
+    if ((n!=-9999)&&(flag==1)&&(n!=2517))    
+         printf("%d ", n);}              
+   return 0;
+}*/
+
+int n=0;
+while (n!=2517){
+    scanf("%d", &n);
+}
+while (n!=-9999)
+{
+scanf("%d", &n);
+if ((n!=-9999)&&(n!=2517))
+printf("%d ", n);}
+}
+
+
