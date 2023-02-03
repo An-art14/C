73fc1d67210638f24e20c6146d37ae6ce8c33c31
anastasiya <an_art14@mail.ru>
2023-02-03 23:41:49 +0700

    add with while

---
+++ wh8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+int n=1, sum=0;
+while (n){
+    scanf("%d", &n); }
+    do {
+scanf("%d", &n);
+    sum=sum+n;     }
+while (n);
+while (n!=-9999){
+    scanf("%d", &n);
+}
+printf("%d", sum);
+    return 0;
+}
